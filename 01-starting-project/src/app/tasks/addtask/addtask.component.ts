3437f66e9ff1c8a15e88c14318764e0ea8213d8e
iT3E <26583558+iT3E@users.noreply.github.com>
2024-07-20 18:50:08 -0700

    NEW: Add forms

--- 01-starting-project/src/app/tasks/addtask/addtask.component.ts
+++ 01-starting-project/src/app/tasks/addtask/addtask.component.ts
@@ -1,16 +1,20 @@
 import { Component, Output, EventEmitter } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-addtask',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './addtask.component.html',
   styleUrl: './addtask.component.css',
 })
 export class AddtaskComponent {
-  @Output() stopaddingtask = new EventEmitter<boolean>();
+  @Output() stopaddingtask = new EventEmitter<void>();
+  enteredTitle = '';
+  enteredSummary = '';
+  enteredDate = '';
 
   onStopAddingTask() {
-    this.stopaddingtask.emit(false);
+    this.stopaddingtask.emit();
   }
 }
